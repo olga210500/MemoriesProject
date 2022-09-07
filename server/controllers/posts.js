50a27f3bcb05140013f9399e26b6c845a169398a
ruslana380900 <ruslanachernenko94@gmail.com>
2022-09-07 23:38:18 +0300

    first commit

---
+++ server/controllers/posts.js
@@ -0,0 +1,27 @@
+import PostMessage from "../models/postMessage.js"
+const getPosts = async (req, res)=>{
+    try {
+        const postMessages = await PostMessage.find() 
+        res.status(200).json(postMessages)
+        console.log(postMessages);
+        
+    } catch (error) {
+        res.status(404).json({message:error.message})
+    }
+    res.send('it works!')
+}
+const createPost = async (req, res)=>{
+    const post = req.body;
+    const newPost = new PostMessage(post)
+    try {
+        await newPost.save();
+        res.status(201).json(newPost)
+    } catch (error) {
+        res.status(409).json({message:error.message})
+
+    }
+    res.send('create post')
+
+}
+
+export { getPosts, createPost}
---
+++ server/index.js
@@ -0,0 +1,16 @@
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import express from 'express';
+import postRoutes from './routes/posts.js';
+const app = express();
+app.use(bodyParser.json({limit:'30mb', extended:true}));
+app.use(bodyParser.urlencoded({limit:'30mb', extended:true}));
+app.use(cors());
+app.use('/posts',postRoutes)
+const CONNECTION_URL = 'mongodb+srv://olha:41upegog@cluster0.6yb8qsc.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL)
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
