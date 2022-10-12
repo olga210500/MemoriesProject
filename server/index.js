d92a1c7c0aeb278ee5216dc42e9c2807471a0f60
olha kovalisko <olha.kovalisko@knameit.com>
2022-10-12 16:44:37 +0300

    quick fixes

--- server/index.js
+++ server/index.js
@@ -3,15 +3,16 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import express from 'express';
 import postRoutes from './routes/posts.js';
-
+import dotenv from 'dotenv';
 const app = express();
 
+dotenv.config();
 app.use(bodyParser.json({limit:'30mb', extended:true}));
 app.use(bodyParser.urlencoded({limit:'30mb', extended:true}));
 app.use(cors());
 app.use('/posts',postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://olha:lnuproject123@cluster0.6yb8qsc.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL)
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,9 @@
 import express from 'express';
 import {getPosts, createPost, updatePost, deletePost} from '../controllers/posts.js';
-import multer from 'multer'
 const router = express.Router();
-const upload = multer({dest:"uploads"});
 
 router.get('/', getPosts);
-router.post('/', upload.single("selectedFile"),createPost)
+router.post('/', createPost)
 router.patch('/:id', updatePost);
 router.delete('/:id', deletePost);
 export default router;
